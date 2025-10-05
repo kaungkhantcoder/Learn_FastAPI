f406931f1db3f67d6efba51e3b34749bd6ee21ae
kaungkhantcoder <kaungkhantcoder@gmail.com>
2025-10-05 15:05:31 +0630

    ⚡ fastapi

--- main.py
+++ main.py
@@ -4,29 +4,8 @@ app = FastAPI()
 
 @app.get('/')
 async def index() -> dict[str, str]:
-    return {'hello': 'world'}
+    return {"Hello, World"}
 
 @app.get('/about')
 async def about() -> str:
-    return 'An Exceptional Company'
-
-@app.get('/me')
-async def me() -> str:
-    return {f"Do you want to know my age? >> {23}"}
-
-@app.get('/bio')
-async def bio() -> list:
-    return [2, 4, 6, 8]
-
-@app.patch('/new')
-async def new() -> list:
-    return "new"
-
-@app.post('/add')
-async def add(item):
-    item_dict = item.model_dump()
-    return {"message": "Item created successfully", "item": item_dict}
-
-@app.delete('/remove', status_code = 204)
-async def remove(item_id: int):
-    return item_id
+    return 'About Our Company'
