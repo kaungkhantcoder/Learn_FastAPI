1b38038f71709f0f581e322e76a7fa04659925da
kaungkhantcoder <kaungkhantcoder@gmail.com>
2025-10-06 23:53:45 +0630

    ⚡ enum

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI, HTTPException
+from enum import Enum
 
 app = FastAPI()
 
+class GenreURLChoices(Enum):
+    ROCK = 'rock'
+    ELECTORNIC = 'electornic'
+    METAL = 'metal'
+    HIP_HOP = 'hip-hop'
+
 BANDS = [
-    {'id': 1, 'name': 'The Kinks', 'genere': 'Rock'},
-    {'id': 2, 'name': 'Aphex Twin', 'genere': 'Electornic'},
-    {'id': 3, 'name': 'Slowdive', 'genere': 'Shoegaze'},
-    {'id': 4, 'name': 'Win-Tang Clan', 'genere': 'Hip-Hop'},
+    {'id': 1, 'name': 'The Kinks', 'genre': 'Rock'},
+    {'id': 2, 'name': 'Aphex Twin', 'genre': 'Electornic'},
+    {'id': 3, 'name': 'Black Sabbath', 'genre': 'Metal'},
+    {'id': 4, 'name': 'Win-Tang Clan', 'genre': 'Hip-Hop'},
 ]
 
 @app.get('/bands')
@@ -21,8 +28,8 @@ async def band(band_id: int) -> dict:
         raise HTTPException(status_code=404, detail="Band Not Found")
     return band
 
-@app.get('bands/genere/{genere}')
-async def bands_for_genere(genere: str) -> list[dict]:
+@app.get('/bands/genre/{genre}')
+async def bands_for_genre(genre: str) -> list[dict]:
     return [
-        b for b in BANDS if b['genere'].lower() == genere.lower()
+        b for b in BANDS if b['genre'].lower() == genre.lower()
     ]
